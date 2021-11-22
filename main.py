d4f7f75a5924eb2235662e8d2f6e0dd56cc3b1d7
Cubgl <74872839+Cubgl@users.noreply.github.com>
2021-11-22 18:55:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print('Hello! This is my first Git Project!')
     print("I'm Olga!")
     print("Version 1.0a")
-    print('How are you?')
+    print('How are you ?')
 
 
 if __name__ == '__main__':
